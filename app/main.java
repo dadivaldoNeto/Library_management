31e108f1c210812aba553f91ccda0f3de6a96309
Dadivaldo Neto Mendonça <128710179+dadivaldoNeto@users.noreply.github.com>
2026-05-10 22:51:25 +0000

    feat: Add function to print User table datas

--- app/main.java
+++ app/main.java
@@ -1,3 +1,6 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import model.*;
 
 public class main {
@@ -6,5 +9,27 @@ public class main {
 		myLivro.showInfo();
 		Client hi = new Client();
 		hi.showInfo();
+
+		connect();
 	}
+
+    public static void connect() {
+        // connection string
+        var url = "jdbc:sqlite:lib.db";
+        var sql = "SELECT * FROM User";
+
+        try (var conn = DriverManager.getConnection(url);
+             var stmt = conn.createStatement();
+             var rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.printf("%-5s%-25s%n",
+                        rs.getInt("id"),
+                        rs.getString("name")
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
